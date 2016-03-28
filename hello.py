5a8b18831d6ce1a96f6775299ee313cf1bb34ef3
chen zhou <chenzhou_cz@163.com>
2016-03-28 10:20:26 +0800

    IMy first one Python

---
+++ hello.py
@@ -0,0 +1,72 @@
+# print('hello word');
+# print('the quick brown fox' ,'jumps over', 'the lazy dog');
+# print('100+200=',100+200);
+# #name=input();
+# #print('hello',name);
+# print('1024*786=',1024*786);
+# #print absolute value of an integer:
+# a=100
+# if a>=0:
+	# print(a)
+# else:
+	# print(-a)
+	
+# print('i\'m\ "OK\"!');
+
+# x=10;
+# x=x+10;
+# print(x);
+# a='abc';
+# b=a;
+# a='cdf';
+# print(a+'分割'+b);
+
+# print('小明的成绩从去年的72分提升到了今年的85分，请计算小明成绩提升的百分点，并用字符串格式化显示出xx.x%，只保留小数点后1位：')
+
+# s1 = 72
+
+# s2 = 85
+
+# r = (s2 - s1)/s2*100
+
+# print('%.1f %%'% r)
+
+# classmates=['Michael','Bol','Tracy']
+#  print(len(classmates))
+#  print(classmates[1]+'第一个')
+#  print(classmates[-1]+'倒数第二个')
+#  for s in classmates:
+	# print(s)
+
+# print('---追加一个到末尾--------------')
+# classmates.append('lily')
+# print(classmates)
+# print('----追加一个到指定位置-----------')
+# classmates.insert(1,'dengdeng')
+# print(classmates)
+# print('---删除末尾的元素--')
+# classmates.pop()
+# print(classmates)
+# print('-------删除指定元素---------------')
+# classmates.pop(1)
+# print(classmates)
+# print('----------替换某个元素----------------')
+# classmates[1]='Sarah'
+# print(classmates)
+
+# #二维数组
+# s=['python','java',['asp','php'],'scheme']
+# print(len(s))
+# #另一种写法
+# p=['asp','php']
+# z=['python','java',p,'scheme']
+# #要拿到php可以写p[1]或者z[2][1]
+# print(z[2][1])
+
+L=[['Apple','Google','Microsoft'],['JAVA','Python','Ruby','PHP'],['Adam','Bart','Lisa']]
+
+print('打印Apple:'+L[0][0])
+print('打印Lisa:'+L[2][2])
+print('打印Python:'+L[1][1])
+
+
