7356ee2e96a265201602dcf26cbd7277ab9fdeec
chen zhou <chenzhou_cz@163.com>
2016-04-05 10:48:42 +0800

    push

--- hello.py
+++ hello.py
@@ -149,7 +149,7 @@
 	# print(sum);
 	# print(n);
 #请利用循环依次对list中的每个名字打印出Hello, xxx!：
-# L = ['Bart', 'Lisa', 'Adam']
-# for name in L:
+ L = ['Bart', 'Lisa', 'Adam']
+ for name in L:
 		# print("hello:"+name)
 
