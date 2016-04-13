2738bdbf3b09bdd1f29bb95fd35c8af0ebc4c9ca
chen zhou <chenzhou_cz@163.com>
2016-04-13 16:29:12 +0800

    test myPyCharm

--- hello.py
+++ hello.py
@@ -1022,5 +1022,5 @@
 # 不需要我们自己定义int2()，可以直接使用下面的代码创建一个新的函数int2：
 import functools
 int2=functools.partial(int,base=2)
-int2('1000000')#输出64
-int2('1010101')#输出85
+print(int2('1000000'))#输出64
+print(int2('1010101'))#输出85
