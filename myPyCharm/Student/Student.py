c9c96e0579253f690a1cd7993d3485329609ccdd
chen zhou <chenzhou_cz@163.com>
2016-04-22 11:14:30 +0800

    枚举

--- myPyCharm/Student/Student.py
+++ myPyCharm/Student/Student.py
@@ -374,4 +374,32 @@
 # print(callable('str'))
 
 # 通过callable()函数，我们就可以判断一个对象是否是“可调用”对象
+#
+# from enum import Enum
+#
+# Month = Enum('Month',('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+#
+# for name, member in Month.__members__.items():
+#     print(name, '==>', member, ',', member.value)
 
+# 如果需要更精确地控制枚举类型，可以从Enum派生出自定义类：
+# from enum import Enum,unique
+#
+# @unique
+# class WeekDay(Enum):
+#     Sun = 0 # Sun的value被设定为0
+#     Mon = 1
+#     Tue = 2
+#     Wed = 3
+#     Thu = 4
+#     Fri = 5
+#     Sat = 6
+# @unique装饰器可以帮助我们检查保证没有重复值
+# 访问这些枚举可以有若干中方法
+# day1 = WeekDay.Mon
+# print(day1)
+# print(WeekDay.Mon.value)
+
+# 可见，既可以用成员名称引用枚举常量，又可以直接根据value的值获得枚举常量
+# 小结
+# Enum可以把一组相关常量定义在一个class中，且class不可变，而且成员可以直接比较
