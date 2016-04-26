c52fca9cee6a74948bfe1186a482500e23f56a7b
chen zhou <chenzhou_cz@163.com>
2016-04-26 16:15:28 +0800

    StringIO and BytesIO

--- myPyCharm/Student/Student.py
+++ myPyCharm/Student/Student.py
@@ -611,6 +611,36 @@
 #     f.write('Hello, world!')
 #     f.write('中国')
 
+# *************************StringIO和BytesIO******************************
+# StringIO顾名思义就是在内存中读写str
+# from io import StringIO
+# f = StringIO()
+# f.write('hello')
+# f.write(' ')
+# f.write('world!')
+# print(f.getvalue())
+# 读取IO
+# f = StringIO('Hello!\nHi!\nGoodbye!')
+# while True:
+#     s = f.readline()
+#     if s =='':
+#         break
+#     print(s.strip())
+# BytesIO
+# StringIO操作只能是str,如果要操作二进制数据，就需要使用BytesIO
+# BytesIO实现了在内存中读写bytes
+# 我们创建一个BytestIO,然后写入一些bytes:
+# from io import BytesIO
+# f = BytesIO()
+# f.write('中文'.encode('utf-8'))
+# print(f.getvalue())
+# 注意，写入的不是str,而是经过UTF-8编码的bytes
+# 和StringIO类似，可以用一个bytes初始化BytesIO,然后，像文件一样读取。
+# from io import BytesIO
+# f = BytesIO(b'\xe4\xb8\xad\xe6\x96\x87')
+# f.read()
+
+
 
 
 
