817d2fdb3fda1a65ef5c65ca577230844450d0f6
chen zhou <chenzhou_cz@163.com>
2016-04-26 15:24:13 +0800

    文件读写

--- myPyCharm/Student/Student.py
+++ myPyCharm/Student/Student.py
@@ -543,10 +543,73 @@
 # Python的错误其实也是class，所有的错误类型都继承自BaseException,所以在使用except时
 # 需要注意的是，它不但捕获该类型的错误，还把其子类也“一网打尽”。
 
+# *******************文件读写**********************
+# Python内置了读写文件的函数，用法和C是兼容的
+# 读写文件前，我们先了解一下，在磁盘上读写文件的功能都是由操作系统提供的，现代操作系统不允许普通程序直接
+# 操作磁盘，所以，读写文件就是请求操作系统打开一个文件对象（通常称为文件描述符），然后，通过操作系统提供
+# 的接口从这个文件对象中读取数据（读文件），或者把数据写入这个文件对象（写文件）
 
+# # 读文件
+# # 要读取文件的模式打开一个文件对象，使用Python内置的open()函数，传人文件名和标示符：
+# f = open('E:\myPython\myNotepad++\gitIsFree.txt', 'r')
+# # 读取文件
+# f.read()
+# # 关闭文件
+# f.close()
 
+# 由于文件读写是都有可能产生IOError，一旦出错，后面的f.close()就不会调用。所以，为了保证无论是否出错都能正确地
+# 关闭文件，我们可以使用try .....finally来实现
+# try:
+#     f = open('E:\myPython\myNotepad++\gitIsFree.txt', 'r')
+#     print(f.read())
+# finally:
+#     if f:
+#         f.close()
+# 但是每次都这么写实在太繁琐，所以，Python引人了with语句来自动帮我们调用close（）方法
+# with open('E:\myPython\myNotepad++\gitIsFree.txt', 'r') as f:
+#     print(f.read())
+# 限制每次读取字节大小，防止内存溢出
+# with open('E:\myPython\myNotepad++\gitIsFree.txt', 'r') as f:
+#     print(f.read(20))
+# # 每次读取一行
+# with open('E:\myPython\myNotepad++\gitIsFree.txt', 'r') as f:
+#     print(f.readline())
+# # 读取所有内容并返回list
+# with open('E:\myPython\myNotepad++\gitIsFree.txt', 'r') as f:
+#    for f1 in f.readlines():
+#        print(f1.strip()) # 把末尾的‘\n’删掉
+# 如果文件很小，read()一次性读取最方便
+# 如果不确定文件大小，反复调用read（size）比较保险
+# 如果配置文件，调用readlines()最方便
 
+# file-like Object
+# 像open()函数返回的这种有个read()方法的对象，在Python中统称为file-like Object
+# 除了file外，还可以是内存的字节流，网络流，自定义流等等。file-like Object不要求从特定类继承
+# 只要写个read()方法就行。
 
+# StringIO就是在内存中创建的file-like Object,常用作临时缓冲。
+
+# 二进制文件
+# 前面讲的默认都是读取文本文件，并且是UTF-8编码的文件，要读取二进制文件，比如图片、视频等等，用'rb'
+# 打开文件即可
+# f = open('/Users/michael/test.jpg', 'rb')
+# f.read()
+# 输出 b'\xff\xd8\xff\xe1\x00\x18Exif\x00\x00...' # 十六进制表示的字节
+# 字符编码
+# 要读取非UTF-8编码的文本文件，需要给open()函数传入encoding参数，例如，读取GBK编码的文件
+# with open('E:\myPython\myNotepad++\gitIsFree.txt', 'r', encoding='UTF-8') as f:
+#     print(f.read())
+
+# 遇到有些编码不规范的文件，你可能会遇到UnicodeDecodeError,因为在文本中可能夹杂了一些非法编码的字符
+# 遇到这种情况，open()函数还接收一个errors参数，表示如果遇到编码错误后如何处理。
+# 最简单的方式是直接忽略
+# with open('E:\myPython\myNotepad++\gitIsFree.txt', 'r', encoding='gbk', errors='ignore') as f:
+#     print(f.read())
+
+# 写文件
+# with open('E:\myPython\myNotepad++\gitIsFree.txt', 'w',encoding='UTF-8',errors='ignore') as f:
+#     f.write('Hello, world!')
+#     f.write('中国')
 
 
 
