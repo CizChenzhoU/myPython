3ce52a791f0c2da870a4b20d44db4b8046841268
chen zhou <chenzhou_cz@163.com>
2016-08-25 17:05:59 +0800

    图形界面

--- myPyCharm/Process/TuXing图形界面.py
+++ myPyCharm/Process/TuXing图形界面.py
@@ -9,4 +9,58 @@
 # Tk会调用操作系统提供的本地GUI接口，完成最终的GUI
 # 所以，我们的代码只需要调用Tkinter提供的接口就可以了。
 
-# 第一个
+# 第一个GUI程序
+# 使用Tkinter十分简单，我们来编写一个GUI版本的"Hello world"
+# 第一步是导入Tkiner包的所有内容
+# from tkinter import *
+# # 第二步是从Frame派生一个Application类，这是所有Widget的父容器
+# class Application(Frame):
+#     def __init__(self,master = None):
+#         Frame.__init__(self,master)
+#         self.pack()
+#         self.createWidgets()
+#
+#     def createWidgets(self):
+#         self.helloLabel = Label(self,text='Hello world!')
+#         self.helloLabel.pack()
+#         self.quitButton = Button(self,text='Quit',command=self.quit)
+#         self.quitButton.pack()
+#
+# # 在GUI中，每个Button,Label、输入框等，都是一个Widget.Frame则是可以容纳其他Widget的Widget,所有的Widget组合
+# # 起来就是一棵树。
+# # pack()方法把Widget加入到父容器中，并实现布局。pack()是最简单的布局，grid()可以实现更复杂的布局。
+# # 在createWidgets()方法中，我们创建了一个Label和一个Button,当Button被点击是，触发self.quit()使程序退出。
+# # 第三步，实例化Application,并启动消息循环：
+# app = Application()
+# #设置窗口标题
+# app.master.title('Hello world')
+# # 主消息循环
+# app.mainloop()
+
+# 输入文本
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class Application(Frame):
+    def __init__(self,master=None):
+        Frame.__init__(self,master)
+        self.pack()
+        self.createWidgets()
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self,text='Hello',command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message','Hello,%s' % name)
+
+app = Application()
+# 设置窗口标题
+app.master.title('Hello World')
+# 主消息循环
+app.mainloop()
+
+# 小结
+# Python内置的Tkinter可以满足基本的GUI程序要求，如果是非常复杂的GUI程序，建议用操作系统原生支持的语言和库来编写
